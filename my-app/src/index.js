714ca96a7ba4604c876079405d217caf14c5b105
VSTERLIKOV <victor.sterlikov@gmail.com>
2021-03-12 00:16:14 +0300

    final version

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -48,7 +48,7 @@ class Board extends React.Component {
 				//value={this.state.squares[i]} 
 				//onClick={() => this.handleClick(i)}
 				value={this.props.squares[i]}
-				onClick={()->this.props.onClick(i)}
+				onClick={()=>this.props.onClick(i)}
 			/>
 			);
 	}
@@ -91,29 +91,53 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null),
 			}],
-			xIsNext: true
+			stepNumber: 0,
+			xIsNext: true,			
 		};
 	}
 	handleClick(i) {
-		const squares = this.state.squares.slice();
+		//const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length-1];
+		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
 			return;
 		}
 		squares[i]=this.state.xIsNext ? 'X' : 'O';
 		this.setState({
-			squares: squares,
-			xIsNext : !this.state.xIsNext
+			history: history.concat([{
+				squares: squares,
+			}]),
+			stepNumber: history.length,
+			xIsNext : !this.state.xIsNext,
+		});
+	}
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step%2) === 0,
 		});
 	}
 	render() {
 		const history = this.state.history;
-		const current = history[history.length-1];
+		//const current = history[history.length-1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
+		const moves = history.map((step,move)=>{
+			const desc = move?
+				'goto move #'+move :
+				'goto game start';
+			return(
+				<li key={move}>
+					<button onClick={()=>this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
 		let status;
 		if (winner) {
 			status = 'Победитель: '+ winner;
 		} else {
-			stasu = 'Следующий игрок: '+ this.state.xIsNext?'X':'O';
+			status = 'Следующий игрок: '+ this.state.xIsNext?'X':'O';
 		}
 		return (
 			<div className="game">
@@ -125,7 +149,7 @@ class Game extends React.Component {
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
-					<ol></ol>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 			);
